af6f9cad1643ae1a0fe340e9fec2c0f8f0ff3f1a
Vinicius Moraes <vmoraes.info@gmail.com>
2025-02-06 23:35:56 +0000

    test aws

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import replace from "@rollup/plugin-replace";
 import "dotenv/config";
 
-const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
